ba43c73d1aac6d556659aa24fb7e0d629bf39f1c
Achille kouassi <patriceachillekouassin@gmail.com>
2022-12-28 00:50:59 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,63 @@
+5 + "34"
+// Prediction:534
+// Actual:534
+
+
+
+5 - "4"
+// Prediction:1
+// Actual:1
+
+
+10 % 5
+// Prediction:
+// Actual:
+
+
+5 % 10
+// Prediction:2
+// Actual:0
+
+"Java" + "Script"
+// Prediction:JavaScrip
+// Actual:JavaScrip
+
+
+" " + " "
+// Prediction:
+// Actual:
+
+" " + 0
+// Prediction:0
+// Actual:0
+
+true + true
+// Prediction:2
+// Actual:2
+
+
+true + false
+// Prediction:1
+// Actual:1
+
+false + true
+// Prediction:1
+// Actual:1
+
+
+false - true
+// Prediction:-1
+// Actual:-1
+
+!true
+// Prediction:true
+// Actual:true
+
+
+3 - 4
+// Prediction:-1
+// Actual:-1
+
+"Bob" - "bill"
+// Prediction:NaN
+// Actual:NaN
